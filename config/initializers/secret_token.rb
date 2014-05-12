670e8691e00407f66f2a7883804296fcf9af053f
ismail <isoakbudak@gmail.com>
2014-05-12 21:12:16 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BusNumbers::Application.config.secret_key_base = 'd2d60d17390f8420c1cce61cc5b8e77c9f5e3f7e8d4967902dd96b0958918ed0c03e36eb8b90b51d68e4b3a4765689f79fdf27b4afe952743af18259eabadb22'
