688657716d845ad0fd64b1a37acaaa0ca9cbfcf8
ismail <isoakbudak@gmail.com>
2014-05-25 01:03:34 +0200

    Added static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 BusNumbers::Application.routes.draw do
-  
+   
+
   resources :products  
   scope "(:locale)", :locale => /en|tr/ do
-    root :to => 'products#index'
+    root  'static_pages#home'
+    match '/help',    to: 'static_pages#help',    via: 'get'
+    match '/about',   to: 'static_pages#about',   via: 'get'
+    match '/contact', to: 'static_pages#contact', via: 'get'
   end
 
 end
